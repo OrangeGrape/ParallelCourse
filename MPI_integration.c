8642900d03b089fe32486a61e7fe55e78f82ee14
OrangeGrape <42825993+OrangeGrape@users.noreply.github.com>
2018-10-17 08:04:05 +0700

    Update MPI_integration.c

--- MPI_integration.c
+++ MPI_integration.c
@@ -2,23 +2,19 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-typedef struct
-{
-  float x;  // real part
-  float y;  // imag part
-} Complex;
-
 
 void main(int argc, char** argv) {
-  int max_iter = 10000;
   int nx = 600;
-  int ny = 600;
-  int i, j, k, nxi, nxf;
-  int data[nx][ny];
-  Complex z, c;
-  float lengthsq, temp;
-  FILE *file;
-
+  int i, nxi, nxf;
+  float xi, xmin =-10, xmax = 10, dx = (xmax-xmin)/nx;
+  float fx[nx], sum,tmp;
+  //initial
+  
+  for(i=0;i<nx;i++){
+    xi = xmin + i*dx;
+    fx[i] = (xi*xi) + (2*xi) + 1;
+  }
+  
   int rank, n, source;
 
 /////////////////////////////////////////////////////////////////
@@ -36,45 +32,18 @@ void main(int argc, char** argv) {
   if (nxf>nx){
     nxf = nx;
   }
-
-  for(i=nxi; i < nxf; i++) {
-    for(j=0; j < ny; j++) {
-      z.x = z.y = 0.0;
-      c.x = (4.0*j - 2.0*nx)/nx;
-      c.y = (4.0*i - 2.0*ny)/ny;
-      k = 0;
-      do {
-        temp = z.x*z.x - z.y*z.y + c.x;
-        z.y = 2.0*z.x*z.y + c.y;
-        z.x = temp;
-        lengthsq = z.x*z.x+z.y*z.y;
-        k++;
-      } while (lengthsq < 4.0 && k < max_iter);
-      if (k == max_iter)
-        k = 0;
-      else
-          k = k%16;
-      data[i][j] = k;
-    }
+  
+  sum = 0;
+  for(i=nxi; i < nxf-1; i++) {
+    sum += (fx[i]+fx[i+1])*(dx/2);
   }
 
 //send result to rank 0
   if(rank != 0){
-
-    for(i=nxi; i < nxf; i++) {
-      for(j=0; j < ny; j++) {
-        MPI_Send (&data[i][j], 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
-      }
-    }
-
+    MPI_Send (&fx[i], 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
   }else if (rank == 0){
-
-    for(i= nx/n +1; i < nx; i++) {
-      for(j=0; j < ny; j++) {
         source = i/(nx/n +1);
         MPI_Recv (&data[i][j], 1, MPI_FLOAT, source, 0, MPI_COMM_WORLD, &status);
-      }
-    }
   }  
 //save data
   if(rank == 0){
